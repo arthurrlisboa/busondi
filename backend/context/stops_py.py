d91a17a614733652e89dc589a58e4bacfb4445e8
helenapato <lenapato.m@gmail.com>
2022-05-12 10:59:02 -0300

    improv: melhorando dicionário de retorno

--- backend/context/stops_py.py
+++ backend/context/stops_py.py
@@ -2,15 +2,12 @@ from backend.database.models.bus_stops import BusStops
 from backend.app import busondi_database, app
 
 def get_stops():
-    # TODO: Pegar lista de pontos do banco de dados e retornar no formato json
-    stops = BusStops.query.all()
-    dict = {}
-    for i in stops:
-        dict[i] = {
-            'stop_lat' : i.stop_lat,
-            'stop_lon': i.stop_lon,
-            'stop_id' : i.stop_id,
-            'stop_name' : i.stop_name
+    all_stops = BusStops.query.all()
+    stops_dict = {}
+    for stop in all_stops:
+        stops_dict[stop.stop_id] = {
+            'stop_lat' : stop.stop_lat,
+            'stop_lon': stop.stop_lon,
+            'stop_name' : stop.stop_name
         }
-
-    return dict
+    return stops_dict

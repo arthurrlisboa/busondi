c3ad317374519c1469c3aaeb1fe8cb83156df673
jacksonns <jackson.guima2@gmail.com>
2022-05-29 20:42:03 -0300

    Lógica inicial de login

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from datetime import timedelta
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SECRET_KEY'] = 'busondi-session-key'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 
 busondi_database = SQLAlchemy(app)
 

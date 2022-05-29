ea3c37561980969451372661a9cc2d20b87010b5
arthur lisboa <arthurlisboac@yahoo.com.br>
2022-05-29 17:17:00 -0300

    componente tela de localização da linha

---
+++ FrontEnd/busondi-front/src/app/screens/localizacao-linha/localizacao-linha.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-localizacao-linha',
+  templateUrl: './localizacao-linha.component.html',
+  styleUrls: ['./localizacao-linha.component.css']
+})
+export class LocalizacaoLinhaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

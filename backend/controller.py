ca88a35b765d48f5b2f1ece6bda9eed57e550f60
jacksonns <jackson.guima2@gmail.com>
2022-05-30 22:03:03 -0300

    Serviço de ordenação de linhas por horário de chegada

--- backend/controller.py
+++ backend/controller.py
@@ -1,5 +1,5 @@
 from backend.domain.models.bus_stops_impl import BusStopsImpl
-from backend.domain.models.bus_schedule_impl import get_arrival_time 
+from backend.domain.models.bus_schedule_impl import BusScheduleImpl
 from backend.domain.models.routes_impl import RoutesImpl
 from backend.domain.models.user_impl import UserImpl
 from flask import jsonify, render_template, session
@@ -35,8 +35,11 @@ def list_routes_from_stop(stop_id):
     }
 
     for entry in routes_list:
-        arrival_time = get_arrival_time(stop_id, entry[1].route_id)
-        routes_dict['stop_routes'][entry[1].route_id] = arrival_time
+        arrival_time = BusScheduleImpl.get_arrival_time(stop_id, entry[1].route_id)
+        if arrival_time != 'None':
+            routes_dict['stop_routes'][entry[1].route_id] = arrival_time
+    routes_dict['stop_routes'] = BusScheduleImpl.order_by_arrival_time(routes_dict['stop_routes'])
+    print(routes_dict['stop_routes'])
 
     return render_template("stops_id.html", routes=jsonify(routes_dict))
 

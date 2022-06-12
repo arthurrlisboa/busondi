d1c53c5e6a442ec047a784a498e7f30c96f44a6f
arthur lisboa <arthurlisboac@yahoo.com.br>
2022-06-12 18:40:53 -0300

    Ajustes para favoritos

--- FrontEnd/busondi-front/src/app/services/favorites.service.ts
+++ FrontEnd/busondi-front/src/app/services/favorites.service.ts
@@ -9,13 +9,6 @@ import { AuthService } from './auth.service';
 export class FavoriteService {
   private favoritesUrl = 'http://localhost:5000/favorites';
 
-  private favoriteLines : LineQuery[] = [
-    { id: '1404', name: '1404', stopId: '128371937', stopName: 'Rua geraldo magela, 150' },
-    { id: '54a', name: '54 A', stopId: '128345937', stopName: 'Rua desenbargador joao neto, 160' },
-    { id: '54b', name: '54 B', stopId: '12567567', stopName: 'Rua bom retiro, 176' },
-    { id: '5628', name: '5628', stopId: '62348889', stopName: 'Avenida Abílio machado, 67' },
-  ];
-
   constructor(
     private http: HttpClient,
     private authService: AuthService
@@ -40,12 +33,6 @@ export class FavoriteService {
 
   addFavorite(id: string, stopId: string){
     this.http.post(this.favoritesUrl, {email: this.authService.getEmail(), route_id: id, stop_id: stopId});
-  }
-
-  removeFavorites( favorite: LineQuery){
-    this.favoriteLines = this.favoriteLines.filter(function(value, index, arr){ 
-      return value.id !== favorite.id && value.stopId !== favorite.stopId;
-    });
   }
 
 }

7963d722fbf7a5686d20bf9c41d3a3404867c898
helenapato <lenapato.m@gmail.com>
2022-06-23 09:02:27 -0300

    feat: mocks para testes de get-route

--- backend/tests/mock/route_stop_repository_mock.py
+++ backend/tests/mock/route_stop_repository_mock.py
@@ -1,3 +1,4 @@
+from backend.database.models.routes import Routes
 from backend.domain.route_stop.route_stop import RouteStop
 
 class RouteStopRepositoryMock:
@@ -8,5 +9,16 @@ class RouteStopRepositoryMock:
                 RouteStop('609-03', 1, None, 0.0, 18568, '10100130600640'),
                 RouteStop('609-03', 1, None, 0.0, 18568, '10100446100580')
             ]
-        else:
-            return 'Invalid Route ID'
+        return 'Invalid Route ID'
+
+    def return_all_routes_from_stop_impl(self, stop_id):
+        if(stop_id == '104787000120'):
+            return [
+                RouteStop('609-01', 43, None, 7983.46, 18401, '104787000120'),
+                RouteStop('609-02', 43, None, 7983.46, 18504, '104787000120'),
+                RouteStop('609-03', 45, None, 9182.09, 18612, '104787000120')
+            ]
+        return 'Invalid Stop ID'
+      
+    def return_route_stop_by_id_impl(self, stop_id, route_id):
+        return 0

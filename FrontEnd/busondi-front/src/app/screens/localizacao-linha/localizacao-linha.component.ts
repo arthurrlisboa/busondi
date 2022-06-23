9bd3e58b61e5a6599c3943865a495ba9e3150e4d
Pedro Henrique <pedrohbertolini@hotmail.com>
2022-06-23 13:30:01 -0300

    localizacao de linha e horario

--- FrontEnd/busondi-front/src/app/screens/localizacao-linha/localizacao-linha.component.ts
+++ FrontEnd/busondi-front/src/app/screens/localizacao-linha/localizacao-linha.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { first, map, Observable, startWith } from 'rxjs';
 import { LocalizarLinhaService } from 'src/app/services/localizar-linha.service';
 
 @Component({
@@ -8,14 +9,44 @@ import { LocalizarLinhaService } from 'src/app/services/localizar-linha.service'
   styleUrls: ['./localizacao-linha.component.css']
 })
 
-export class LocalizacaoLinhaComponent{
-
+export class LocalizacaoLinhaComponent implements OnInit{
+  stopOptions = new Array<Stop>();
+  filteredDepartureOptions: Observable<Stop[]>;
   updateForm = this.fb.group({
     departure: ['', Validators.required]
   })
 
   constructor(private fb: FormBuilder, private locationService: LocalizarLinhaService) { 
-    this.updateForm.controls['departure'].setValue(locationService.getDeparture());
+    this.updateForm.controls['departure'].setValue({stop_name: locationService.getDeparture(), stop_id: locationService.getDepartureId()});
+  }
+
+  ngOnInit(): void {
+    this.locationService.getRouteStops(this.locationService.getLineId())
+      .pipe(first())
+      .subscribe({
+        next: (itens) => {
+          this.stopOptions = itens.stops
+        }
+      })
+
+    this.filteredDepartureOptions = this.updateForm.controls["departure"].valueChanges.pipe(
+      startWith(''),
+      map(value => (typeof value === 'string' ? value : value?.stop_name)),
+      map(name => (name ? this._filterDeparture(name) : this.stopOptions.slice())),
+    );
+  }
+
+  private _filterDeparture(value: string): Stop[] {
+    const filterValue = value.toLowerCase();
+    if(this.stopOptions.length){
+      let foundStop = this.stopOptions.filter(option => option.stop_name?.toLowerCase().includes(filterValue));
+      return [...foundStop];
+    }
+    else return []
+  }
+
+  displayFnStop(stop: Stop) {
+    return stop && stop.stop_name ? stop.stop_name : ""
   }
 
   getLine(){
@@ -33,4 +64,12 @@ export class LocalizacaoLinhaComponent{
     this.locationService.updateLine(idDeparture, departureName);
     console.warn(this.updateForm.value);
   }
+}
+
+
+export interface Stop{
+  stop_id?: string;
+  stop_lat?: number;
+  stop_lon?: number;
+  stop_name?: string;
 }

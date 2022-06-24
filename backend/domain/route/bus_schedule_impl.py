beb5c6d5ef65ebf468f4e04052674a13ae380ac8
helenapato <lenapato.m@gmail.com>
2022-06-24 09:22:39 -0300

    feat: nova classe para BusDeparture

--- backend/domain/route/bus_schedule_impl.py
+++ backend/domain/route/bus_schedule_impl.py
@@ -38,24 +38,24 @@ class BusScheduleImpl:
                                 seconds=round(time_obj.second,2))
 
     def get_arrival_time_impl(self, stop_id, route_id):
-        travel_time_timedelta = BusScheduleImpl().get_travel_time_timedelta(stop_id, route_id)
+        travel_time_timedelta = self.get_travel_time_timedelta(stop_id, route_id)
 
         trip_list = self.routes_repo.return_trips_from_route(route_id)
-        current_time = BusScheduleImpl().get_current_time_timedelta()
+        current_time = self.get_current_time_timedelta()
         best_arrival_time = None
         for trip in trip_list:
             time_obj = trip.departure_time
-            departure_time = BusScheduleImpl().get_timedelta_from_time_obj(time_obj)
+            departure_time = self.get_timedelta_from_time_obj(time_obj)
             arrival_time = departure_time + travel_time_timedelta
             if (arrival_time > current_time and (not best_arrival_time or arrival_time < best_arrival_time)):
                 best_arrival_time = arrival_time
                 
         return (str(best_arrival_time) or '')
     
-    def order_by_arrival_time_impl(self, routes_dict):
+    #def order_by_arrival_time_impl(self, routes_dict):
         # Converte o valor do dicionário de str para timedelta
-        timedelta_dict = dict( (x, [routes_dict[x][0], datetime.datetime.strptime(routes_dict[x][1],"%H:%M:%S.%f")] ) for x in routes_dict )
+        #timedelta_dict = dict( (x, [routes_dict[x][0], datetime.datetime.strptime(routes_dict[x][1],"%H:%M:%S.%f")] ) for x in routes_dict )
         # Ordena dicionário conforme timedelta
-        sorted_dict = dict( sorted( timedelta_dict.items(), key= lambda x: BusScheduleImpl().get_timedelta_from_time_obj(x[1][1]) ) )
+        #sorted_dict = dict( sorted( timedelta_dict.items(), key= lambda x: self.get_timedelta_from_time_obj(x[1][1]) ) )
         # Retorna dicionário convertendo timedelta para string novamente
-        return dict( (x, [sorted_dict[x][0], str(sorted_dict[x][1])[11:16] ] ) for x in sorted_dict )
+        #return dict( (x, [sorted_dict[x][0], str(sorted_dict[x][1])[11:16] ] ) for x in sorted_dict )

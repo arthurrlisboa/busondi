0f184867f0b253daca3b418143343d7fd9b82743
helenapato <lenapato.m@gmail.com>
2022-06-26 11:50:23 -0300

    feat: nova função no mock de UserRepository

--- backend/tests/mock/user_repository_mock.py
+++ backend/tests/mock/user_repository_mock.py
@@ -3,7 +3,11 @@ from backend.database.models.user import User
 class UserRepositoryMock:
 
     def return_all_users_impl(self):
-        return 0
+        return [
+            User('maria@email.com', '12345', 'Maria'),
+            User('joao@email.com', '12345', 'Joao'),
+            User('laura@email.com', '12345', 'Laura')
+        ]
 
     def add_user_impl(self, user):
         return 0

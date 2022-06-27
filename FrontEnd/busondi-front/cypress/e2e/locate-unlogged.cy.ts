9034190ee7f68efe177b25ba21a9dba9672a59b2
arthur lisboa <arthurlisboac@yahoo.com.br>
2022-06-26 22:36:14 -0300

    Primeiro caso de teste - Line location unlogged

---
+++ FrontEnd/busondi-front/cypress/e2e/locate-unlogged.cy.ts
@@ -0,0 +1,34 @@
+
+describe('Search line departure time', () => {
+  it('Visits the initial project page', () => {
+    cy.visit('/')
+    cy.contains('Localize seus ônibus de forma prática')
+    cy.contains('Salve e acesse facilmente suas linhas favoritas')
+  })
+
+  it('Visits the locate line page', () => {
+    cy.get('[data-cy=menu]').click()
+    cy.get('[data-cy=locate-line]').click()
+    cy.get('[data-cy=menu]').click()
+  });
+
+  it('Locate the line departure time', () => {
+    cy.get('[data-cy=line]').type("1404")
+    cy.contains('1404A - Palmeiras / Alipio De Melo (Principal)').click()
+    cy.get('[data-cy=departure]').type("Rua")
+    cy.contains('Rua Curitiba 656').click();
+    cy.get('[data-cy=submit]').click()
+  });
+
+  it('Check resulta page', () => {
+    cy.contains('O próximo ônibus da linha 1404A chegará no ponto de partida às')
+  })
+
+  it('Updates location request with other departure', () => {
+    cy.get('[data-cy=departure]').clear().type("Rua")
+    cy.contains('Curitiba 862').click();
+    cy.get('[data-cy=submit]').click();
+    cy.contains('O próximo ônibus da linha 1404A chegará no ponto de partida às')
+  })
+
+});

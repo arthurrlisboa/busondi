c081bba322f1d7f4811615f5f3440f2070f7f87f
helenapato <lenapato.m@gmail.com>
2022-06-28 09:21:14 -0300

    refact: list comprehension

--- backend/domain/bus_stop/get_stop_impl.py
+++ backend/domain/bus_stop/get_stop_impl.py
@@ -8,9 +8,7 @@ class GetStopImpl:
 
     def get_all_stops_impl(self):
         all_stops = self.stops_repo.return_all_stops()
-        stops_list = []
-        for stop in all_stops:
-            stops_list.append(BusStop(stop.stop_id, stop.stop_name, stop.stop_lat, stop.stop_lon))
+        stops_list = [BusStop(stop.stop_id, stop.stop_name, stop.stop_lat, stop.stop_lon) for stop in all_stops]
         return stops_list
 
     def get_stop_by_id_impl(self, stop_id):
@@ -20,16 +18,12 @@ class GetStopImpl:
 
     def get_stops_coordinates_impl(self, stops_list):
         stops_in_list = self.stops_repo.return_all_stops_in_list(stops_list)
-        coordinate_list = []
-        for stop in stops_in_list:
-            coordinate_list.append((stop.stop_lon, stop.stop_lat))
+        coordinate_list = [(stop.stop_lon, stop.stop_lat) for stop in stops_in_list]
         return coordinate_list
 
     def get_stops_from_route_impl(self, route_id):
         stops_in_route = self.route_stop_repo.return_all_stops_in_route(route_id)
         stop_id_list = [stop.stop_id for stop in stops_in_route]
         stops = self.stops_repo.return_all_stops_in_list(stop_id_list)
-        stops_list = []
-        for stop in stops:
-            stops_list.append(BusStop(stop.stop_id, stop.stop_name, stop.stop_lat, stop.stop_lon))
+        stops_list = [BusStop(stop.stop_id, stop.stop_name, stop.stop_lat, stop.stop_lon) for stop in stops]
         return stops_list
